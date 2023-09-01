b72a689a350895b3ac41a78ea0ba21b00502afb4
Osniel Cebrero Arcos <osnielca@gmail.com>
2023-08-31 18:42:06 -0600

    mejora

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,77 +1,72 @@
-// Espera a que el DOM se haya cargado completamente
 document.addEventListener("DOMContentLoaded", () => {
-    // Selecciona el elemento de la barra de navegación
+    // Seleccionar elementos del DOM
     const navbar = document.querySelector('.navbar');
-    // Botón de menú móvil y los enlaces de navegación
     const mobileMenuToggle = document.getElementById("mobile-menu");
     const navLinks = document.getElementById("navLinks");
-    // Texto oculto
     const hiddenText = document.querySelector(".hidden-text");
-    // Todos los enlaces del menú de navegación
     const links = document.querySelectorAll('nav ul li a');
-    // Excluye el último enlace del array de enlaces
     const excludedLink = links[links.length - 1];
-
-    // Filtra los enlaces para excluir el último
     const filteredLinks = Array.from(links).filter(link => link !== excludedLink);
 
-    // Agrega un evento de clic a cada enlace del menú de navegación (excepto el último)
-    filteredLinks.forEach(link => {
-        link.addEventListener('click', function (event) {
-            event.preventDefault();
-            // Obtiene el atributo 'href' del enlace
-            const targetId = link.getAttribute('href');
-            // Desplaza suavemente a la sección correspondiente
-            document.querySelector(targetId).scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
-    });
+    // Función para desplazarse suavemente a una sección
+    function scrollToSection(targetId) {
+        document.querySelector(targetId).scrollIntoView({ behavior: 'smooth' });
+    }
 
-    // Muestra el texto oculto después de 1 segundo
-    setTimeout(() => {
+    // Mostrar el texto oculto después de un tiempo
+    function showHiddenText() {
         hiddenText.classList.add("show-text");
-    }, 1000);
+    }
 
-    // Agrega un evento de clic al botón de menú móvil
-    mobileMenuToggle.addEventListener("click", () => {
-        // Alterna la clase 'active' en el botón de menú móvil
+    // Alternar el menú móvil
+    function toggleMobileMenu() {
         mobileMenuToggle.classList.toggle("active");
-        // Muestra u oculta los enlaces de navegación
         navLinks.classList.toggle("show");
-    });
+    }
 
-    // Agrega un evento de desplazamiento de ventana
-    window.addEventListener('scroll', function () {
-        // Si el desplazamiento vertical de la ventana es mayor que 0
-        if (window.scrollY > 0) {
-            // Agrega la clase 'scroll' a la barra de navegación
-            navbar.classList.add('scroll');
-        } else {
-            // Si el desplazamiento es 0, elimina la clase 'scroll' de la barra de navegación
-            navbar.classList.remove('scroll');
-        }
-    });
-
-    function toggleItem(item) {
-        if (window.innerWidth <= 768) { // Dispositivo móvil
+    // Expandir o contraer elementos de la galería en dispositivos móviles
+    function toggleGalleryItem(item) {
+        if (window.innerWidth <= 768) {
             item.classList.toggle('expanded');
         }
     }
 
-    // Obtén todos los elementos con la clase 'item'
+    // Manejar el clic en un enlace del menú de navegación
+    function handleLinkClick(event, targetId) {
+        event.preventDefault();
+        scrollToSection(targetId);
+    }
+
+    // Manejar interacciones con elementos de la galería
+    function handleGalleryInteraction(item) {
+        toggleGalleryItem(item);
+    }
+
+    // Manejar el desplazamiento de la ventana
+    function handleScroll() {
+        const shouldAddScrollClass = window.scrollY > 0;
+        navbar.classList.toggle('scroll', shouldAddScrollClass);
+    }
+
+    // Agregar manejadores de eventos a los enlaces del menú de navegación
+    filteredLinks.forEach(link => {
+        const targetId = link.getAttribute('href');
+        link.addEventListener('click', event => handleLinkClick(event, targetId));
+    });
+
+    // Mostrar el texto oculto después de un retraso
+    setTimeout(showHiddenText, 1000);
+    // Agregar un manejador de clic al botón de menú móvil
+    mobileMenuToggle.addEventListener("click", toggleMobileMenu);
+    // Agregar un manejador de desplazamiento de ventana
+    window.addEventListener('scroll', handleScroll);
+
+    // Obtener todos los elementos de la galería y agregar manejadores de eventos
     const galleryItems = document.querySelectorAll('.item');
 
-    // Agrega el evento de clic y el evento de paso del cursor (mouseover)
     galleryItems.forEach(item => {
-        item.addEventListener('click', function () {
-            toggleItem(this);
-        });
-        item.addEventListener('mouseover', function () {
-            toggleItem(this);
-        });
-        item.addEventListener('mouseout', function () {
-            toggleItem(this);
-        });
+        item.addEventListener('click', () => handleGalleryInteraction(item));
+        item.addEventListener('mouseover', () => handleGalleryInteraction(item));
+        item.addEventListener('mouseout', () => handleGalleryInteraction(item));
     });
 });
